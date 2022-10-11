1cd552e691ac26364eaa99f48284e1a8100a5428
John Richter <johndayrichter@google.com>
2022-10-11 09:26:26 -0600

    OSS license headers, bad terminology fixes

--- opr-models/build-tools/compileschema.ts
+++ opr-models/build-tools/compileschema.ts
@@ -1,10 +1,45 @@
 #!/usr/bin/env node
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import {Command} from 'commander';
 import {compile, JSONSchema} from 'json-schema-to-typescript';
 import fs from 'fs';
 import path from 'path';
 import process from 'process';
 import glob from 'glob-promise';
+
+const LICENSE_HEADER =
+  '/**\n' +
+  ' * Copyright 2022 Google LLC\n' +
+  ' *\n' +
+  ' * Licensed under the Apache License, Version 2.0 (the "License");\n' +
+  ' * you may not use this file except in compliance with the License.\n' +
+  ' * You may obtain a copy of the License at\n' +
+  ' *\n' +
+  ' *      http://www.apache.org/licenses/LICENSE-2.0\n' +
+  ' *\n' +
+  ' * Unless required by applicable law or agreed to in writing, software\n' +
+  ' * distributed under the License is distributed on an "AS IS" BASIS,\n' +
+  ' * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,' +
+  ' either express or implied.\n' +
+  ' * See the License for the specific language governing permissions and\n' +
+  ' * limitations under the License.\n' +
+  ' */\n';
+
 const program = new Command();
 
 program
@@ -127,6 +162,8 @@ async function main() {
   compiledTypes.unshift(
     '/* DO NOT EDIT - Automatically generated from json schema files */\n'
   );
+  compiledTypes.unshift(LICENSE_HEADER);
+  schemaImports.unshift(LICENSE_HEADER);
 
   if (options.typefile) {
     fs.writeFileSync(
