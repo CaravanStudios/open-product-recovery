7d32d79797aec309c2f5c892b73baffc9ba959fd
Mike Ryckman <ryckman@google.com>
2023-01-18 12:34:53 -0700

    update references and erroneous type

--- integrations/google-cloud/src/pubsub/pubsub.ts
+++ integrations/google-cloud/src/pubsub/pubsub.ts
@@ -16,65 +16,62 @@
 
 import {
   TenantNodeIntegrationInstaller,
+  TenantNodeIntegrationContext,
   IntegrationApi,
   OfferChange,
   OfferChangeType,
   PluggableFactory,
-  PluggableFactorySet,
 } from 'opr-core';
-import {TenantNodeIntegrationContext} from 'opr-core/build/config/hostintegrationcontext';
 import {PubSub, ClientConfig, ServiceError} from '@google-cloud/pubsub';
 
 export const PubSubIntegration = {
-  pubsub: {
-    async construct<Allowed extends PluggableFactorySet>(
-      json: PubSubConfig,
-      context: TenantNodeIntegrationContext
-    ): Promise<TenantNodeIntegrationInstaller> {
-      return {
-        type: 'integrationInstaller',
+  async construct(
+    json: PubSubConfig,
+    context: TenantNodeIntegrationContext
+  ): Promise<TenantNodeIntegrationInstaller> {
+    return {
+      type: 'integrationInstaller',
 
-        async install(api: IntegrationApi): Promise<void> {
-          const postCallback = (err: ServiceError | null) => {
-            if (err) {
-              console.error(`Pubsub Integration Error: ${err}`);
+      async install(api: IntegrationApi): Promise<void> {
+        const postCallback = (err: ServiceError | null) => {
+          if (err) {
+            console.error(`Pubsub Integration Error: ${err}`);
+          }
+        };
+
+        const changeHandler = async (change: OfferChange) => {
+          if (!json.events || json.events.includes(change.type)) {
+            try {
+              const pubsub = new PubSub(json.clientConfig);
+              const topic = pubsub.topic(json.topicNameOrId);
+              const data = JSON.stringify({
+                hostUrl: context.hostOrgUrl,
+                changeType: change.type,
+                timestampUTC: change.timestampUTC,
+                offerId: change.newValue?.id ?? change.oldValue?.id,
+                offer: change.newValue ?? change.oldValue,
+              });
+              topic.publishMessage(
+                {
+                  data: Buffer.from(data),
+                },
+                postCallback
+              );
+            } catch (error) {
+              console.log(`Pubsub Error: ${error}`);
             }
-          };
+          }
+        };
 
-          const changeHandler = async (change: OfferChange) => {
-            if (!json.events || json.events.includes(change.type)) {
-              try {
-                const pubsub = new PubSub(json.clientConfig);
-                const topic = pubsub.topic(json.topicNameOrId);
-                const data = JSON.stringify({
-                  hostUrl: context.hostOrgUrl,
-                  changeType: change.type,
-                  timestampUTC: change.timestampUTC,
-                  offerId: change.newValue?.id ?? change.oldValue?.id,
-                  offer: change.newValue ?? change.oldValue,
-                });
-                topic.publishMessage(
-                  {
-                    data: Buffer.from(data),
-                  },
-                  postCallback
-                );
-              } catch (error) {
-                console.log(`Pubsub Error: ${error}`);
-              }
-            }
-          };
-
-          api.registerChangeHandler(changeHandler);
-        },
-      };
-    },
-  } as PluggableFactory<
-    TenantNodeIntegrationInstaller,
-    PubSubConfig,
-    TenantNodeIntegrationContext
-  >,
-};
+        api.registerChangeHandler(changeHandler);
+      },
+    };
+  },
+} as PluggableFactory<
+  TenantNodeIntegrationInstaller,
+  PubSubConfig,
+  TenantNodeIntegrationContext
+>;
 
 export interface PubSubConfig {
   clientConfig: ClientConfig;
