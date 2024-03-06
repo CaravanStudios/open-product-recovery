3e19b75de89b318bd9bc29677551f78849506417
Mike Ryckman <mryckman@gmail.com>
2024-03-06 13:21:55 -0700

    upgrade lerna, add tweak setup, add rebuild step

--- setup.sh
+++ setup.sh
@@ -3,5 +3,9 @@ sh ./clean.sh
 # when that package's install script finishes. This forces the install command
 # to not run the post-install scripts. So we then separately run the compile.
 # This is discussed here: https://github.com/npm/cli/issues/3034
-npm install --workspaces --ignore-scripts
+npm install --workspaces --ignore-scripts --include-workspace-root
+# npm install -D prettier eslint-config-prettier eslint-plugin-prettier
 npm run compile --workspaces
+npm rebuild
+
+
